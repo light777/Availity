97e1c7de618b316b08c2aebe64335b884e5381e0
Matthew Oberlin <Oberlin.M@gmail.com>
2020-08-10 15:09:17 -0500

    Code Validator code

---
+++ Code Validator/Code_Validator/Validator.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Code_Validator
+{
+	public class Validator
+	{
+		public static bool CheckParanthesis(string code)
+		{
+			int openParens = 0;
+
+			code = StripComments(code);
+			foreach(var c in code)
+			{
+				if(c == '(')
+				{
+					openParens++;
+				}
+				else if(c == ')')
+				{
+					if(openParens > 0)
+					{
+						openParens--;
+					}
+					else
+					{
+						// Closing paren found before opening paren
+						return false;
+					}
+				}
+			}
+
+			return openParens == 0;
+		}
+
+		public static string StripComments(string code)
+		{
+			StringBuilder sb = new StringBuilder();
+			using (StringReader sr = new StringReader(code))
+			{
+				string line;
+				while ((line = sr.ReadLine()) != null)
+				{
+					if(!line.Trim().StartsWith(";;"))
+					{
+						sb.AppendLine(line);
+					}
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ Code Validator/Code_Validator_Tests/ValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code_Validator_Tests
+{
+	[TestClass]
+	public class ValidatorTests
+	{
+		[TestMethod]
+		public void TestGoodCode()
+		{
+			var result = Code_Validator.Validator.CheckParanthesis(@"
+				(defun csg-intersection-intersect-all (obj-a obj-b)
+				 (lambda (ray)
+					 (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+						 (merge 'fvector
+										(remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+										(remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+										#'<))))
+			");
+
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void TestBadCode()
+		{
+			var result = Code_Validator.Validator.CheckParanthesis(@"
+				(defun csg-intersection-intersect-all (obj-a obj-b)
+				 (lambda (ray)
+					 (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+						 (merge 'fvector
+										(remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+										(remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+										#'<))
+			");
+
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void TestClosingParenBeforeOpeningParen()
+		{
+			var result = Code_Validator.Validator.CheckParanthesis(@"
+				))(defun csg-intersection-intersect-all (obj-a obj-b)
+				 (lambda (ray)
+					 (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+						 (merge 'fvector
+										(remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+										(remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+										#'<))))
+			");
+
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void TestCodeWithComments()
+		{
+			var result = Code_Validator.Validator.CheckParanthesis(@"
+				;; A mistake will increase by a factor of 3 while a correct answer
+				;; will decrease by a factor of unity
+				(defvar *mult-hyst* 3)
+			");
+		}
+
+		[TestMethod]
+		public void TestCodeWithParensInComments()
+		{
+			var result = Code_Validator.Validator.CheckParanthesis(@"
+				;; A mistake will increase by a factor of 3) while a correct answer
+				;; will decrease by a factor of unity
+				(defvar *mult-hyst* 3)
+			");
+		}
+	}
+}
